39684fac6597ea2de5c339d05d1ccc7f11de6783
HwnagJaecheon <jchwang@btsoft.kr>
2021-01-04 14:39:05 +0900

    객체지향 쿼리 언어1 - 기본 문법 - 소개 - 기본 문법과 쿼리 API

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -1,9 +1,6 @@
 package jpql;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import javax.persistence.*;
 import java.util.List;
 
 /**
@@ -21,6 +18,19 @@ public class JpaMain {
     tx.begin();
 
     try {
+      Member member = new Member();
+      member.setUsername("member1");
+      member.setAge(10);
+      em.persist(member);
+
+      Member result = em.createQuery("select m from Member m where m.username = :username", Member.class)
+          .setParameter("username", "member1")
+          .getSingleResult();
+
+      System.out.println("singleResult = " + result);
+
+
+
 
 
       tx.commit();  // [트랜잭션] 커밋
